2d752f30dac1491f6a3d835ae132a2c1db99b800
cyanplatypus <julia.a.andr@gmail.com>
2019-04-10 21:51:24 +0300

    + http interceptor: add token to request

--- conference-spa/src/app/app.module.ts
+++ conference-spa/src/app/app.module.ts
@@ -4,10 +4,11 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
-import { HttpClient, HttpClientModule} from 'node_modules/@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS} from 'node_modules/@angular/common/http';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import {ErrorInterceptorProvider } from './_services/error.interceptor';
+import {TokenHttpRequestInterceptor } from './_services/token-http-request.interceptor';
 
 
 @NgModule({
@@ -24,7 +25,13 @@ import {ErrorInterceptorProvider } from './_services/error.interceptor';
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      {
+         provide: HTTP_INTERCEPTORS,
+         useClass: TokenHttpRequestInterceptor,
+         multi: true
+       }
+      
    ],
    bootstrap: [
       AppComponent
