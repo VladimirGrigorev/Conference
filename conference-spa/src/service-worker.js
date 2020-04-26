c0c991bffa26308976d0b4b5eab2cb57a8619b18
Grigorev Vladimir <grigorevvladimir1999@gmail.com>
2020-04-26 17:59:52 +0300

    Add service-worker, static cache

---
+++ conference-spa/src/service-worker.js
@@ -0,0 +1,22 @@
+﻿﻿const staticCacheName = 'static-cache-v0';
+
+const staticAssets = [
+  'assets/js/app.js'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(staticAssets);
+  console.log('Service worker has been installed');
+});
+
+self.addEventListener('activate', async event => {
+  console.log('Service worker has been activated');
+});
+
+self.addEventListener('fetch', event => {
+  console.log(`Trying to fetch ${event.request.url}`);
+  event.respondWith(caches.match(event.request).then(cachedResponse => {
+    return cachedResponse || fetch(event.request)
+  }));
+});
